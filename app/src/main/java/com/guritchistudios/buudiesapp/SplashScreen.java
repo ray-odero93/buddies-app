f6d57e05ef3135573491c7f7a8073aacec6d9f69
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-08 15:20:02 +0300

    This will check if user already has an account or not. In case the user already has an account  it will retrieve said account.

--- app/src/main/java/com/guritchistudios/buudiesapp/SplashScreen.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/SplashScreen.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        if (mAuth != null) {
+            currentUser = mAuth.getCurrentUser();
+        }
     }
 }
