3dab2aa1cb54a7307e1bfa16b1026999ed78ba9e
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-08 15:41:15 +0300

    This will ensure if user does not have account, he/she will be directed to the login page. In case user has account, he/she will be directed to the main page of app(TL).

--- app/src/main/java/com/guritchistudios/buudiesapp/SplashScreen.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/SplashScreen.java
@@ -2,7 +2,9 @@ package com.guritchistudios.buudiesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,5 +20,22 @@ public class SplashScreen extends AppCompatActivity {
         if (mAuth != null) {
             currentUser = mAuth.getCurrentUser();
         }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user == null) {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();;
+                } else {
+                    Intent mainIntent = new Intent(SplashScreen.this, DashboardActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();;
+                }
+            }
+        }, 1000);
     }
 }
