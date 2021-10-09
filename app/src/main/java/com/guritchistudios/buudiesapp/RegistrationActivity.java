25e60886f6ee1f5cc66d02e2e958163959803b9d
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-09 11:03:26 +0300

    Declare properties to contain referenced values from RegistrationActivity

--- app/src/main/java/com/guritchistudios/buudiesapp/RegistrationActivity.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/RegistrationActivity.java
@@ -2,9 +2,20 @@ package com.guritchistudios.buudiesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText name, email, password;
+    private Button mRegister;
+    private TextView existingAccount;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
