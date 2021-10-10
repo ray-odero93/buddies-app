88d640d9a127b17540a28aac754b2dcdc24311e8
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-10 14:53:40 +0300

    Declare properties for holding refernced layout elements.

--- app/src/main/java/com/guritchistudios/buudiesapp/LoginActivity.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/LoginActivity.java
@@ -3,8 +3,21 @@ package com.guritchistudios.buudiesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText name, email, password;
+    private Button mLogin;
+    private TextView needNewAccount, recoveryPass;
+    FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private ProgressBar loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
