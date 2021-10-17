2bc564f4082a195dbb7c456d11b99edf70ee4710
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-17 17:13:18 +0300

    Set an event listener on the edit profile picture button.

--- app/src/main/java/com/guritchistudios/buudiesapp/EditProfilePage.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/EditProfilePage.java
@@ -1,18 +1,25 @@
 package com.guritchistudios.buudiesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -44,7 +51,7 @@ public class EditProfilePage extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile_page);
 
         editName = findViewById(R.id.edit_name);
-        editPass = findViewById(R.id.change_password);
+        editPass = findViewById(R.id.change_pass);
         profilePic = findViewById(R.id.profile_pic);
         set = findViewById(R.id.setting_profile_image);
         dialog = new ProgressDialog(this);
@@ -56,5 +63,41 @@ public class EditProfilePage extends AppCompatActivity {
         reference = database.getReference("Users");
         storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
         cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        Query query = reference.child("email").equalTo(firebaseUser.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1:snapshot.getChildren()) {
+                    String image = "" + snapshot1.child("image").getValue();
+                    try {
+                        Glide.with(EditProfilePage.this).load(image).into(set);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        editPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               dialog.setMessage("Changing password...");
+               showPassChangeDialog();
+            }
+        });
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.setMessage("Updating profile photo...");
+                profileOrCoverPic = "image";
+                showImagePicDialog();
+            }
+        });
     }
 }
