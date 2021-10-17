ec9709dd07131ddedc5f300696fb23b5b7250d59
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-17 09:20:41 +0300

    Declare properties for holding values.

--- app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
@@ -1,10 +1,21 @@
 package com.guritchistudios.buudiesapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
+    String myUid;
+    ActionBar actionBar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
