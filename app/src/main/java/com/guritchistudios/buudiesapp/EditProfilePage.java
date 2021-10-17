0b515095441a13e5003da1f7d81bdf6ee9156d64
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-17 16:53:17 +0300

    Link declared properties to relevant views of the activity, database & storage paths; users and camera.

--- app/src/main/java/com/guritchistudios/buudiesapp/EditProfilePage.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/EditProfilePage.java
@@ -2,6 +2,7 @@ package com.guritchistudios.buudiesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class EditProfilePage extends AppCompatActivity {
@@ -40,5 +42,19 @@ public class EditProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_page);
+
+        editName = findViewById(R.id.edit_name);
+        editPass = findViewById(R.id.change_password);
+        profilePic = findViewById(R.id.profile_pic);
+        set = findViewById(R.id.setting_profile_image);
+        dialog = new ProgressDialog(this);
+        dialog.setCanceledOnTouchOutside(false);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        reference = database.getReference("Users");
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     }
 }
