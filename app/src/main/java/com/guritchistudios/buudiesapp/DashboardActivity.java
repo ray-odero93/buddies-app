2e7e6289b85a5fcddf7a3e28b62b3b60adbc396e
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-17 09:33:52 +0300

    Set page title on actionbar and home fragment as landing page upon navigating to the dashboard page.

--- app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
@@ -1,9 +1,12 @@
 package com.guritchistudios.buudiesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,5 +24,23 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile Activity");
+        mAuth = FirebaseAuth.getInstance();
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(selectedListener);
+
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, fragment, "");
+        transaction.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            return false;
+        }
     }
 }
