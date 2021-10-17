89089807e5555a7950384eaa2363ae1b53b1f927
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-10-17 09:50:20 +0300

    Enable navigating from one fragment to another.

--- app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
+++ app/src/main/java/com/guritchistudios/buudiesapp/DashboardActivity.java
@@ -40,6 +40,47 @@ public class DashboardActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.content, fragment, "");
+                    transaction.commit();
+                    return true;
+
+                case R.id.nav_users:
+                    actionBar.setTitle("Users");
+                    UsersFragment fragment1 = new UsersFragment();
+                    FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+                    transaction1.replace(R.id.content, fragment1, "");
+                    transaction1.commit();
+                    return true;
+
+                case R.id.nav_add_blogs:
+                    actionBar.setTitle("Add");
+                    AddBlogsFragment fragment2 = new AddBlogsFragment();
+                    FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                    transaction2.replace(R.id.content, fragment2, "");
+                    transaction2.commit();
+                    return true;
+
+                case R.id.nav_chat:
+                    actionBar.setTitle("Chat");
+                    ChatListFragment fragment3 = new ChatListFragment();
+                    FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                    transaction3.replace(R.id.content, fragment3, "");
+                    transaction3.commit();
+                    return true;
+
+                case R.id.nav_profile:
+                    actionBar.setTitle("Profile");
+                    ProfileFragment fragment4 = new ProfileFragment();
+                    FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
+                    transaction4.replace(R.id.content, fragment4, "");
+                    transaction4.commit();
+                    return true;
+            }
             return false;
         }
     }
